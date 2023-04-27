4f5a6566a871ee49e2f3dba978be332a4d3f8758
hiphop5782 <zdlsz@hanmail.net>
2023-04-27 23:56:18 +0900

    태국 달력 문제

---
+++ judge/src/judge/q18108/Main.java
@@ -0,0 +1,13 @@
+package judge.q18108;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int year = sc.nextInt();
+		sc.close();
+		year -= 543;
+		System.out.println(year);
+	}
+}
