b0eaf8a2697ec11750266b8790307a5984360db6
hiphop5782 <zdlsz@hanmail.net>
2023-04-27 17:43:47 +0900

    upload

---
+++ judge/src/judge/q1008/Main.java
@@ -0,0 +1,13 @@
+package judge.q1008;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+        System.out.println((double)a / b);
+    }
+}
---
+++ judge/src/judge/q10926/Main.java
@@ -0,0 +1,12 @@
+package judge.q10926;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nickname = sc.next();
+        sc.close();
+        System.out.println(nickname.concat("??!"));
+    }
+}
