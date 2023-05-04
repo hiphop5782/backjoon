f243d4e09c3945df9c81964959724767d34467ce
hiphop5782 <zdlsz@hanmail.net>
2023-05-04 18:07:49 +0900

    ascii 변환프로그램

---
+++ judge/src/judge/q11654/Main.java
@@ -0,0 +1,14 @@
+package judge.q11654;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String alpha = sc.next();
+		sc.close();
+		
+		char ch = alpha.charAt(0);
+		System.out.println((int)ch);
+	}
+}
